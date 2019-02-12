920b87bddfaa95e22c434475e5a3e0de83b5a035
Ambarish Kumar <36997739+ambarishK@users.noreply.github.com>
2019-02-12 07:59:23 -0800

    Update script.sh

--- crops/rice/script.sh
+++ crops/rice/script.sh
@@ -1,7 +1,7 @@
-ami-dictionary create --hreftext --input https://en.wikipedia.org/wiki/Rice --informat wikipage --dictionary Rice --outformats xml,json,html
+ami-dictionary create --input https://en.wikipedia.org/wiki/Rice --informat wikipage --dictionary Rice --outformats xml,json,html
 
-ami-dictionary create --hreftext --input https://en.wikipedia.org/wiki/Category:Rice_varieties --informat wikicategory --dictionary Rice_varieties_category --outformats xml,json,html
+ami-dictionary create --input https://en.wikipedia.org/wiki/Category:Rice_varieties --informat wikicategory --dictionary Rice_varieties_category --outformats xml,json,html
 
-ami-dictionary create --hreftext --input https://en.wikipedia.org/wiki/Category:Oryza --informat wikicategory --dictionary Oryza_category --outformats xml,json,html
+ami-dictionary create --input https://en.wikipedia.org/wiki/Category:Oryza --informat wikicategory --dictionary Oryza_category --outformats xml,json,html
 
-ami-dictionary create --hreftext --input https://en.wikipedia.org/wiki/List_of_rice_varieties --informat wikipage --dictionary Oryza_category --outformats xml,json,html
+ami-dictionary create --input https://en.wikipedia.org/wiki/List_of_rice_varieties --informat wikipage --dictionary Oryza_category --outformats xml,json,html
