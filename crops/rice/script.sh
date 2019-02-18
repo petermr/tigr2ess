bd32e51b9d2c2ea75ae57b0410a25d43a9ffeed3
Ambarish Kumar <36997739+ambarishK@users.noreply.github.com>
2019-02-18 11:46:04 +0530

    Update script.sh

--- crops/rice/script.sh
+++ crops/rice/script.sh
@@ -1,8 +1,18 @@
 ami-dictionary create --input https://en.wikipedia.org/wiki/Rice --informat wikipage --dictionary Rice --outformats xml,json,html
-# Operation and options
+# create - Operation and options
 #  --input - wikipedia page, wikipedia list or wikipedia category page web-link used as input. 
+# --informat - option for web-link address for wikipedia page.
+# -- dictionary - option for dictionary name.
+# --outformat - option for output file format.
 ami-dictionary create --input https://en.wikipedia.org/wiki/Category:Rice_varieties --informat wikicategory --dictionary Rice_varieties_category --outformats xml,json,html
-
+# create - Operation and options
+#  --input - wikipedia page, wikipedia list or wikipedia category page web-link used as input. 
+# --informat - option for web-link address for wikipedia page.
+# -- dictionary - option for dictionary name.
+# --outformat - option for output file format.
 ami-dictionary create --input https://en.wikipedia.org/wiki/Category:Oryza --informat wikicategory --dictionary Oryza_category --outformats xml,json,html
-
-ami-dictionary create --input https://en.wikipedia.org/wiki/List_of_rice_varieties --informat wikipage --dictionary Oryza_category --outformats xml,json,html
+# create - Operation and options
+#  --input - wikipedia page, wikipedia list or wikipedia category page web-link used as input. 
+# --informat - option for web-link address for wikipedia page.
+# -- dictionary - option for dictionary name.
+# --outformat - option for output file format.ami-dictionary create --input https://en.wikipedia.org/wiki/List_of_rice_varieties --informat wikipage --dictionary Oryza_category --outformats xml,json,html
