fdede174cff286bae6935f65880399c93631b893
Peter Murray-Rust <peter.murray.rust@googlemail.com>
2019-02-28 07:19:49 +0000

    add examples dictionaries

---
+++ dictionaries/examples/abiotic.sh
@@ -0,0 +1,42 @@
+#! /bin/sh
+# abiotic terms scraped from various Wikipedia articles and journals. Peter Murray-Rust, 2019
+ami-dictionary create \
+	--description "abiotic terms scraped from various Wikipedia articles and journals. Peter Murray-Rust, 2019" \
+	 --terms \
+ "abiotic"\
+ "abscisic acid"\
+ "ABA"\
+ "cell wall stress"\
+ "chloroplast stress"\
+ "climate"\
+ "cold"\
+ "compaction"\
+ "contamination"\
+ "drought"\
+ "edaphic"\
+ "ER stress"\
+ "extreme temperature"\
+ "flood"\
+ "global warming"\
+ "heavy metals"\
+ "high radiation"\
+ "high wind"\
+ "hyperosmotic stress"\
+ "ion toxicity"\
+ "ion toxicityoxidative stress"\
+ "metal"\
+ "mitochondrionstress"\
+ "natural disaster"\
+ "osmotic stress"\
+ "peroxisome stress"\
+ "pH"\
+ "phosphate"\
+ "precipitation"\
+ "rock content"\
+ "salinity"\
+ "salinization"\
+ "serpentine"\
+ "stress"\
+ "tornado"\
+ "wildfire"\
+	  --dictionary abiotic --directory mydictionaries --outformats xml,html
