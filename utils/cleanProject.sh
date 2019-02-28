10a055a329005389b303e06021d0a475bd372ff7
Peter Murray-Rust <peter.murray.rust@googlemail.com>
2019-02-28 07:11:34 +0000

    created abiotic dictionary

--- utils/cleanProject.sh
+++ utils/cleanProject.sh
@@ -2,11 +2,14 @@
 # cleans cproject
 # leaves EPMC metadata , */fulltext.xml
 
+# run from CProject directory (e.g. osanctum200)
+
 # HTML from XML
 rm */scholarly.html
 # per-tree results
 rm */*.snippets.xml
 rm */*.count.xml
+rm -rf search.*.xml word.*.xml species.*.xml
 rm -rf */results
 # cooccurrence
 rm -rf __cooccurrence/
